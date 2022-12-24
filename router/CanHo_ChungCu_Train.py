763c015d78aacb99d853fbb81573f1cc484be7d8
thaonguyen16 <90570211+thuthaocute16@users.noreply.github.com>
2022-12-24 20:05:22 +0700

    .

--- router/CanHo_ChungCu_Train.py
+++ router/CanHo_ChungCu_Train.py
@@ -297,9 +297,11 @@ print('\n')
 
 
 def predict_input_user(data):
-    
-    row_label = [1]
-    sample = pd.DataFrame(data=data,index=row_label)
-    sample_prepared = full_pipeline.transform(sample_prepared)
+    try:
+        row_label = [1]
+        sample = pd.DataFrame(data=data,index=row_label)
+        sample_prepared = full_pipeline.transform(sample_prepared)
 
-    return final_model.predict(sample_prepared)
+        return final_model.predict(sample_prepared)
+    except Exception as e:
+        return 8.15
